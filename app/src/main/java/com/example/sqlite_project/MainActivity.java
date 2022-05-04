f72776bc99595c447381280205c31b5e38dfeea6
Ahmad Hamoush <hamoush@outlook.com>
2022-05-04 22:18:17 +0300

    Displayed Exams List

--- app/src/main/java/com/example/sqlite_project/MainActivity.java
+++ app/src/main/java/com/example/sqlite_project/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.sqlite_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,25 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         ArrayList data = new ArrayList<>();
-
+        //where the exams will be stored
+         ArrayList exams = new ArrayList<>();
+         //getting list view
         ListView listView = (ListView) findViewById(R.id.exams_list);
-
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, data);
-
+                android.R.layout.simple_list_item_1, exams);
         listView.setAdapter(adapter);
 
-
         try{
-
+            //creating local db
             SQLiteDatabase sql = this.openOrCreateDatabase("examsDB", MODE_PRIVATE, null);
+            //inserting exams into db
             sql.execSQL("CREATE Table IF NOT EXISTS  exams (name VARCHAR, date Date)");
             sql.execSQL("INSERT INTO exams(name,date) VALUES('Mobile Computing','09-05-2022')");
             sql.execSQL("INSERT INTO exams(name,date) VALUES('Natural Language Processing','12-05-2022')");
             sql.execSQL("INSERT INTO exams(name,date) VALUES('Intro to Bioinformatics','10-05-2022')");
-        
-
+            //getting exam details
+            Cursor cursor = sql.rawQuery("SELECT * FROM exams", null);
+            int exam_name = cursor.getColumnIndex("name");
+            int exam_date = cursor.getColumnIndex("date");
+            cursor.moveToFirst();
+            //displaying exams list
+            while (cursor != null) {
+                String exam = "Exam: " +  cursor.getString(exam_name) + "\nDate:  " + cursor.getString(exam_date);
+                exams.add(exam);
+                cursor.moveToNext();
+            }
 
             listView.setAdapter(adapter);
 
